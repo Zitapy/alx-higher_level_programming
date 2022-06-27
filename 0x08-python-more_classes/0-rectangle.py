9c8c6da39301e469fdc4d449d60d95c05dc1f9a6
root <estherzita1993@gmail.com>
2022-06-27 14:35:23 -0700

    commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""""This program creates an empty rectangle class"""
+
+
+class Rectangle():
+    """
+    pass attribute - empty rectangle class
+    """
+    def _init_(self):
+        pass
